bfb46b7ed72df9e62d4f13138ceda3515a23d006
MR Wirth <wirth.mr@gmail.com>
2026-03-26 19:40:25 -0400

    Section 2.8.

--- src/ch2-first-test/__tests__/password-verifier1.spec.js
+++ src/ch2-first-test/__tests__/password-verifier1.spec.js
@@ -178,3 +178,142 @@ describe("v6: PasswordVerifier1", () => {
     });
   });
 });
+
+// As before, but with factory methods replacing some of the
+// `beforeEach()`s, making it a bit more readable.
+describe("v7: PasswordVerifier1", () => {
+  let verifier;
+  beforeEach(() => (verifier = new PasswordVerifier1()));
+  describe("with a failing rule", () => {
+    let errors;
+    beforeEach(() => {
+      verifier.addRule(makeFailingRule("fake reason"));
+      errors = verifier.verify("any value");
+    });
+    it("has an error message based on the rule.reason", () => {
+      expect(errors[0]).toContain("fake reason");
+    });
+    it("has exactly one error", () => {
+      expect(errors.length).toBe(1);
+    });
+  });
+  describe("with a passing rule", () => {
+    let errors;
+    beforeEach(() => {
+      verifier.addRule(makePassingRule());
+      errors = verifier.verify("any value");
+    });
+    it("has no errors", () => {
+      expect(errors.length).toBe(0);
+    });
+  });
+  describe("with a failing and a passing rule", () => {
+    let errors;
+    beforeEach(() => {
+      verifier.addRule(makePassingRule());
+      verifier.addRule(makeFailingRule("fake reason"));
+      errors = verifier.verify("any value");
+    });
+    it("has one error", () => {
+      expect(errors.length).toBe(1);
+    });
+    it("error text belongs to failed rule", () => {
+      expect(errors[0]).toContain("fake reason");
+    });
+  });
+
+  // Placed internally so as not to collide with other examples.
+  const makeFailingRule = (reason) => {
+    return (input) => ({ passed: false, reason: `${reason} | ${input}` });
+  };
+
+  const makePassingRule = () => (input) => {
+    return { passed: true, reason: "" };
+  };
+});
+
+// As before, but with factory methods replacing all of the
+// `beforeEach()`s, making it a bit more readable.
+const makeVerifier = () => new PasswordVerifier1();
+const passingRule = (input) => ({ passed: true, reason: "" });
+
+const makeVerifierWithPassingRule = () => {
+  const verifier = makeVerifier();
+  verifier.addRule(passingRule);
+  return verifier;
+};
+
+const makeVerifierWithFailedRule = (reason) => {
+  const verifier = makeVerifier();
+  const fakeRule = (input) => ({
+    passed: false,
+    reason: `${reason} | ${input}`,
+  });
+  verifier.addRule(fakeRule);
+  return verifier;
+};
+
+describe("v8: PasswordVerifier1", () => {
+  describe("with a failing rule", () => {
+    it("has an error message based on the rule.reason", () => {
+      const verifier = makeVerifierWithFailedRule("fake reason");
+      const errors = verifier.verify("any value");
+      expect(errors[0]).toContain("fake reason");
+    });
+    it("has exactly one error", () => {
+      const verifier = makeVerifierWithFailedRule("fake reason");
+      const errors = verifier.verify("any value");
+      expect(errors.length).toBe(1);
+    });
+  });
+  describe("with a passing rule", () => {
+    it("has no errors", () => {
+      const verifier = makeVerifierWithPassingRule();
+      const errors = verifier.verify("any value");
+      expect(errors.length).toBe(0);
+    });
+  });
+  describe("with a failing and a passing rule", () => {
+    it("has one error", () => {
+      const verifier = makeVerifierWithFailedRule("fake reason");
+      verifier.addRule(passingRule);
+      const errors = verifier.verify("any value");
+      expect(errors.length).toBe(1);
+    });
+    it("error text belongs to failed rule", () => {
+      const verifier = makeVerifierWithFailedRule("fake reason");
+      verifier.addRule(passingRule);
+      const errors = verifier.verify("any value");
+      expect(errors[0]).toContain("fake reason");
+    });
+  });
+});
+
+// v9: Abandon `describe()`, return to `test()`.
+test("PasswordVerifier1, with a failing rule, has an error message based on the rule.reason", () => {
+  const verifier = makeVerifierWithFailedRule("fake reason");
+  const errors = verifier.verify("any value");
+  expect(errors[0]).toContain("fake reason");
+});
+test("PasswordVerifier1, with a failing rule, has exactly one error", () => {
+  const verifier = makeVerifierWithFailedRule("fake reason");
+  const errors = verifier.verify("any value");
+  expect(errors.length).toBe(1);
+});
+test("PasswordVerifier1, with a passing rule, has no errors", () => {
+  const verifier = makeVerifierWithPassingRule();
+  const errors = verifier.verify("any value");
+  expect(errors.length).toBe(0);
+});
+test("PasswordVerifier1, with a failing and a passing rule, has one error", () => {
+  const verifier = makeVerifierWithFailedRule("fake reason");
+  verifier.addRule(passingRule);
+  const errors = verifier.verify("any value");
+  expect(errors.length).toBe(1);
+});
+test("PasswordVerifier1, with a failing and a passing rule, error text belongs to failed rule", () => {
+  const verifier = makeVerifierWithFailedRule("fake reason");
+  verifier.addRule(passingRule);
+  const errors = verifier.verify("any value");
+  expect(errors[0]).toContain("fake reason");
+});
