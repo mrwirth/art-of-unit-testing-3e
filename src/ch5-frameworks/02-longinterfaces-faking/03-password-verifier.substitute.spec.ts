4a5740e59a41ac17c5ba8538a7e8f38a6adb4b67
Matthew Wirth <matthewwirth@cloudfitsoftware.com>
2026-04-07 18:29:15 -0400

    Section 5.4.2.

---
+++ src/ch5-frameworks/02-longinterfaces-faking/03-password-verifier.substitute.spec.ts
@@ -0,0 +1,19 @@
+import { Substitute, Arg } from "@fluffy-spoon/substitute";
+import type { IComplicatedLogger } from "./interfaces/complicated-logger.ts";
+import { PasswordVerifier } from "./00-password-verifier.ts";
+
+describe("working with long interfaces", () => {
+  describe("password verifier", () => {
+    test("verify, w/ logger & passing, calls logger w/ PASS", () => {
+      const mockLogger = Substitute.for<IComplicatedLogger>();
+
+      const verifier = new PasswordVerifier([], mockLogger);
+      verifier.verify("anything");
+
+      mockLogger.received().info(
+        Arg.is((x) => x.includes("PASSED")),
+        "verify",
+      );
+    });
+  });
+});
